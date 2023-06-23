3405b461a82fea43b3c47324ec184dc37667468a
m4arthu <luisarthir259@gmail.com>
2023-06-22 21:46:44 -0300

    projeto finalizado

---
+++ src/app.js
@@ -0,0 +1,54 @@
+const express = require("express")
+const db = require("./db.json")
+const cors = require("cors")
+const bodyParser = require("body-parser")
+const PORT = 5000
+const app = express()
+
+app.use(cors())
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(express.json())
+
+var tweets = [
+]
+var userData = {}
+
+
+app.post("/sign-up", (req, res) => {
+     if (!req.body.username || !req.body.avatar) {
+          res.status(400).send("preencha todos os campos!!")
+     }
+     userData = req.body
+     console.log("userdata:", userData)
+     res.status(200).send("OK")
+})
+app.post("/tweets", (req, res) => {
+     if (!req.body.username || !req.body.tweet) {
+          res.status(400).send("preencha todos os campos!!")
+          return
+     }
+     let tweet = {
+          username: req.body.username,
+          avatar: userData.avatar,
+          tweet: req.body.tweet
+     }
+     tweets.push(tweet)
+     res.status(200).send("OK")
+})
+
+app.get("/tweets", (req, res) => {
+     if (tweets.length <= 10) {
+          res.status(200).send(tweets)
+          console.log("tweets", tweets)
+     } else {
+          let Tweets = []
+          for(i=0 ; i< 10; i++){
+               Tweets.push(tweets[i])
+          }
+          console.log("tweets-reduzidos", Tweets)
+          res.status(200).send(Tweets)
+     }
+})
+
+
+app.listen(`${PORT}`, () => console.log(`server rodando  na porta ${PORT} !!`))
